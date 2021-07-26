ad74ac0800bc87a9c243018fd1b3f46d5f7c1bf2
Devon Wolfkiel <devonwolf@tutanota.com>
2021-07-25 19:13:36 -0700

    Add placeholder tests for major areas of logic

---
+++ src/__tests__/api.test.ts
@@ -0,0 +1,5 @@
+describe.skip('Nexus API', () => {
+    it('is not a test', () => {
+        console.log('no tests yet');
+    });
+});
---
+++ src/__tests__/mods.test.ts
@@ -0,0 +1,5 @@
+describe.skip('Mods', () => {
+    it('is not a test', () => {
+        console.log('no tests yet');
+    });
+});
