2a39e7eeacb51f31607dbd3482ab38ffd8e15fec
Devon Wolfkiel <devonwolf@tutanota.com>
2021-07-31 11:36:01 -0700

    Add failing test for user signup

--- src/__tests__/users.test.ts
+++ src/__tests__/users.test.ts
@@ -1,5 +1,27 @@
-describe.skip('Users', () => {
-    it('is not a test', () => {
-        console.log('no tests yet');
+import request from 'supertest';
+import setup from '../data/setup';
+import app from '../lib/app';
+import pool from '../lib/utils/pool';
+
+describe('Users', () => {
+    beforeEach(() => {
+        return setup(pool);
+    });
+
+    it('creates a new user', () => {
+        const newUser = {
+            email: 'testuser@test.com',
+            password: 'testpassword'
+        };
+
+        return request(app)
+            .post('/api/v1/auth/signup')
+            .send(newUser)
+            .then(response => {
+                expect(response.body).toEqual({
+                    id: '1',
+                    email: newUser.email
+                });
+            });
     });
 });
