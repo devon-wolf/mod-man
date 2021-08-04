4c3d509d1a2bbbc8125d845e5f635f2d84395332
Devon Wolfkiel <devonwolf@tutanota.com>
2021-08-04 16:02:21 -0700

    Refactor payload/userId assignment

--- src/lib/middleware/auth-handling.ts
+++ src/lib/middleware/auth-handling.ts
@@ -2,20 +2,23 @@ import { NextFunction, Response } from 'express';
 import { DynamicRequest } from '../../types';
 import { verify } from '../utils/jwt';
 
-// based the Alchemy foundations BE bootstrap
-
 const checkAuth = (req: DynamicRequest, res: Response, next: NextFunction): void  => {
     const token = req.get('Authorization');
     if (token) {
         try {
             const payload = verify(token);
-            if (typeof payload === 'object') req.userId = payload.id;
+            
+            typeof payload === 'string'
+                ? req.userId = payload
+                : req.userId = payload.id;
+            
+            next();
         }
+        
         catch (error) {
             res.status(401).send({ message: 'Invalid token' });
             return;
         }
-        next();
     }
     else {
         res.status(401).send({ message: 'Request requires a token' });
