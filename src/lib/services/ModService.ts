df73d0f3c8cf345677934586ebd82ca1b11fb15e
Devon Wolfkiel <devon.wolfkiel@gmail.com>
2021-08-14 15:59:45 -0700

    Initial cleanup of mod services

--- src/lib/services/ModService.ts
+++ src/lib/services/ModService.ts
@@ -6,48 +6,37 @@ import { getModById } from '../utils/nexus';
 
 // TODO Consider bypassing service layer for the pieces that just pass along the results from the model
 export default class ModService {
+    
     static async add({ gameDomain, modId }: ModRequest, userId: string): Promise<ModSummary| ErrorMessage | void>  {
+        if(!process.env.NEXUS_API_KEY) return ({ message: 'No API key provided' });
+        
+        const nexusMod = await getModById(gameDomain, modId, process.env.NEXUS_API_KEY);
+        await GameService.add(nexusMod.domain_name);
+        const modExists = await Mod.checkExists(modId);
 
-        if (process.env.NEXUS_API_KEY) {
-            const nexusMod = await getModById(gameDomain, modId, process.env.NEXUS_API_KEY);
-            
-            await GameService.add(nexusMod.domain_name);
-
-            const mod = await Mod.insert(nexusMod, userId);
-            return mod;
+        let mod;
+        if (!modExists) {
+            mod = await Mod.insert(nexusMod, userId);
         }
-        else return ({ message: 'No API key provided' });
+        else {
+            mod = await Mod.update(modExists.id, nexusMod);
+        }
+        return mod;
     }
 
-    static async getAll(userId: string): Promise<ModSummary[] | void> {
-        try {
-            const mods = await UserMod.getUserMods(userId);
-
-            return mods;
-        }
-        catch (error) {
-            console.log(error);
-        }
+    static async getAll(userId: string): Promise<ModSummary[]> {
+        const mods = await UserMod.getUserMods(userId);
+        return mods;
     }
 
-    static async getById(userId: string, modId: string): Promise<ModSummary | void> {
-        try {
-            const mod = await UserMod.getUserModById(userId, modId);
-            return mod;
-        }
-        catch (error) {
-            console.log(error);
-        }
+    static async getById(userId: string, modId: string): Promise<ModSummary> {
+        const mod = await UserMod.getUserModById(userId, modId);
+        return mod;
     }
 
-    static async update(userId: string, modId: string, currentVersion: string): Promise<UserModSummary | void> {
-        try {
-            const mod = await UserMod.updateUserMod(userId, modId, currentVersion);
-            return mod;
-        }
-        catch (error) {
-            console.log(error);
-        }
+    static async update(userId: string, modId: string, currentVersion: string): Promise<UserModSummary> {
+        const mod = await UserMod.updateUserMod(userId, modId, currentVersion);
+        return mod;
     }
 
     static async remove(userId: string, modId: string): Promise<ErrorMessage> {
